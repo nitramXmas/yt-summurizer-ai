7f1738a0719d5f268b45344b1f085e2a5ab76dc6
Martin NOEL <martin.noel.fr@gmail.com>
2024-01-10 19:03:27 +0100

    express server ajustments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import test from "./components/Test"
-import Test from "./components/Test"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -30,7 +28,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <Test/>
     </>
   )
 }
