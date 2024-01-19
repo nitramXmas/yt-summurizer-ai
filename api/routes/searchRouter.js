38d5e63338630bf260324c02a1de3c9da941873b
Martin NOEL <martin.noel.fr@gmail.com>
2024-01-19 05:37:08 +0100

    works with mistralAi

--- api/routes/searchRouter.js
+++ api/routes/searchRouter.js
@@ -1,13 +1,17 @@
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
-const OpenAI = require("openai")
-const bodyParser = require("body-parser");
-const getSubtitles = require('youtube-captions-scraper').getSubtitles;
+import express from 'express'
+import axios from 'axios'
+import OpenAI from "openai"
+import MistralClient from '@mistralai/mistralai'
+import bodyParser from "body-parser"
+import cors from "cors";
+import { getSubtitles } from "youtube-captions-scraper"
 
-const openai = new OpenAI();
+const openAi = new OpenAI();
 const ytApiKey = process.env.YT_API_KEY;
 const ytApiUrl = "https://www.googleapis.com/youtube/v3";
+
+const mistralApiKey = process.env.MISTRAL_API_KEY;
+const mistralAi = new MistralClient(mistralApiKey);
 
 const searchRouter = express.Router();
 
@@ -61,13 +65,21 @@ searchRouter.post("/", async (req, res, next) => {
       resultArray.push(concatenatedText.trim());
     });
 
-    // Summarize using OpenAI API
-    const summarizedResults = await openai.chat.completions.create({
-        messages: [{ role: "system", content: `You are a helpful buying assistant that help me to summerize all thoses reviews from youtube : ${resultArray.join('\n')}. The summery MUST be in french with a first part describing the product. A second part comparing to the concurence if the videos speakes about. A third part with bullets pros and cons and a finall part as a bying advice` }],
+    // Summarize using AI API
+    /**
+     * OpenAI Version
+     * 
+    const summarizedResults = await openAi.chat.completions.create({
+         messages: [{ role: "user", content: `As a buying assistant help me to summerize product reviews from Youtube : ${resultArray.join('\n')}.Summery MUST be in french with 4 parts : product presentation, concurrence, pros / cons and bying advice` }],
         model: "gpt-3.5-turbo-1106",
+     });
+    */
 
+     const summarizedResults = await mistralAi.chat({
+      model : 'mistral-tiny',
+      messages: [{ role: "user", content: `As a buying assistant help me to summerize product reviews from Youtube : ${resultArray.join('\n')}.Summery MUST be in french with 4 parts : product presentation, concurrence, pros / cons and bying advice` }],
+     });
 
-    });
     res.status(200).send({ summarizedText: summarizedResults.choices[0].message.content});
   } catch (err) {
     console.error(`Error while fetching search data for : ${err}`);
@@ -75,4 +87,4 @@ searchRouter.post("/", async (req, res, next) => {
   }
 });
 
-module.exports = searchRouter;
+export default searchRouter;
